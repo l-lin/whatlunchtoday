49517da8f637e51e60ddafd71759a59cfc33d5b0
Louis LIN <louis.lin.87@gmail.com>
2014-04-28 14:04:35 +0200

    Add font-awesome with meteorite + Various change

--- client/resto/resto.js
+++ client/resto/resto.js
@@ -7,17 +7,43 @@ Template.restoList.loading = function() {
 };
 
 Template.restoList.restoList = function() {
-    return RestoList.find({}, {sort: {score: -1}});;
+    return RestoList.find({}, {sort: {score: -1}});
 };
 
 Template.resto.events({
-    'click': function() {
+    'click .wlt-resto': function() {
         if (typeof this.score === 'undefined') {
             this.score = 1;
         } else {
             this.score++;
         }
         RestoList.update(this._id, {$set: {score: this.score}});
+        return false;
+    }
+});
+
+Template.restoForm.events({
+    'click .wlt-form-button': function() {
+        $('.wlt-form').show();
+        $('.wlt-form input[name="name"]').focus();
+        return false;
+    },
+    'click .wlt-form .wlt-cancel': function() {
+        $('.wlt-form').hide();
+        return false;
+    },
+    'submit': function() {
+        var $form = $('.wlt-form'),
+            $name = $('.wlt-form input[name="name"]'),
+            name = $name.val();
+        if (name) {
+            RestoList.insert({
+                name: name,
+                score: 0
+            });
+            $name.val('');
+        }
+        $form.hide();
         return false;
     }
 });
