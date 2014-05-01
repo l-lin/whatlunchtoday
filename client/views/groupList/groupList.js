13a7b1b61b6ba387686a29147a0d59ad5ecdbf1b
Louis LIN <louis.lin.87@gmail.com>
2014-05-01 15:27:05 +0200

    Create/Edit/Remove groups

--- client/views/groupList/groupList.js
+++ client/views/groupList/groupList.js
@@ -1,3 +1,6 @@
+var FORM_TYPE_CREATE = 'Create',
+    FORM_TYPE_MODIFY = 'Modify';
+
 Template.groupList.helpers({
     show: function() {
         var me = Router.current().data().currentUser;
@@ -10,6 +13,7 @@ Template.groupList.helpers({
         return GroupList.find({}, {sort: {name: 1}});
     }
 });
+
 
 Template.groupList.events({
     'click .wlt-form-name': function(event) {
@@ -34,28 +38,61 @@ Template.groupList.events({
         var groupName = this.name;
         if (groupName) {
             var $groupForm = $('.wlt-group-form'),
-                $groupFormName = $groupForm.find('input[type="text"]');
+                $groupFormName = $groupForm.find('input[type="text"]'),
+                $groupFormType = $groupForm.find('input[name="formType"]'),
+                $groupFormPrevValue = $groupForm.find('input[name="prevValue"]');
             $groupForm.show();
             $groupFormName.val(groupName);
+            $groupFormPrevValue.val(groupName);
+            $groupFormType.val(FORM_TYPE_MODIFY);
             $groupFormName.focus();
         }
     },
     'click .wlt-group-create': function(event) {
-        var $groupForm = $('.wlt-group-form');
-        $groupForm.toggle();
-        $groupForm.find('input[type="text"]').focus();
+        var $groupForm = $('.wlt-group-form'),
+            $groupFormName = $groupForm.find('input[type="text"]'),
+            $groupFormType = $groupForm.find('input[name="formType"]'),
+            $groupFormPrevValue = $groupForm.find('input[name="prevValue"]');
+        $groupFormPrevValue.val('');
+        $groupFormType.val(FORM_TYPE_CREATE);
+        $groupForm.show();
+        $groupFormName.val('');
+        $groupFormName.focus();
         event.preventDefault();
     },
     'submit': function() {
-        var $groupName = $('.wlt-group-form').find('input[type="text"]'),
-            groupName = $groupName.val();
+        var $groupForm = $('.wlt-group-form'),
+            $groupFormName = $groupForm.find('input[type="text"]'),
+            $groupFormType = $groupForm.find('input[name="formType"]'),
+            $groupFormPrevValue = $groupForm.find('input[name="prevValue"]');
+        var groupName = $groupFormName.val();
         if (groupName) {
             var group = GroupList.findOne({name: groupName});
             if (!group) {
-                GroupList.insert({name: groupName});
-                $groupName.val('');
+                switch ($groupFormType.val()) {
+                    case FORM_TYPE_MODIFY:
+                        var prevGroup = GroupList.findOne({name: $groupFormPrevValue.val()});
+                        if (prevGroup) {
+                            GroupList.update(prevGroup._id, {$set: {name: groupName}});
+                        }
+                        break;
+                    case FORM_TYPE_CREATE:
+                        GroupList.insert({name: groupName});
+                        break;
+                }
+                $groupFormName.val('');
+                $groupForm.hide();
+            } else {
+                $groupFormName.focus();
             }
         }
         return false;
+    },
+    'click .wlt-group-form .wlt-cancel': function(event) {
+        var $groupForm = $('.wlt-group-form'),
+            $groupFormName = $groupForm.find('input[type="text"]');
+        $groupForm.hide();
+        $groupFormName.val('');
+        event.preventDefault();
     }
 });
